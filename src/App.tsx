b58a95c2d4d412e920a45996a3bb1e65494835be
Scott Maitland <scott.maitland@verint.com>
2022-07-13 22:23:21 +0100

    bulk changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, useState } from 'react';
 import './App.css';
+import { ITask } from './interfaces';
+import TodoTask from './Components/todoTask';
 
-function App() {
+const App = () => {
+
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value));
+    }
+  }
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, taskDeadline: deadline };
+    setTodoList([...todoList, newTask]);
+    setTask("")
+    setDeadline(0)
+  }
+
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(todoList.filter((task) => {
+      return task.taskName !== taskNameToDelete
+    }))
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="header">
+        <div className="inputContainer">
+          <input
+            type="text"
+            placeholder="Task..." 
+            name="task" 
+            value={task}
+            onChange={handleChange} />
+          <input 
+            type="number" 
+            placeholder="Deadline (in Days)..." 
+            name="deadline" 
+            value={deadline}
+            onChange={handleChange} />
+        </div>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="todoList">
+        {todoList.map((task: ITask, key: number) => {
+          return <TodoTask key={key} task={task} completeTask={completeTask}/>
+        })}
+      </div>
+    </div >
   );
-}
+};
 
 export default App;
